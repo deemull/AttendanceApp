7837f2ad5da5a9aacbf96877c4d1890fc164415f
Derrick Mullins <deemullins22@gmail.com>
2018-01-19 10:37:52 -0600

    Update welcome message

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,22 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        // output welcome message
         System.out.println("Welcome to Derrick's World! ");
 
+        // update welcome message with name
         Scanner readInput = new Scanner(System.in);
         System.out.print("What is your name? ");
         String name = readInput.next();
         System.out.printf("Hello %s", name);
+
+        // initialize ArrayList
+        
 
     }
 }
